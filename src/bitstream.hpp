a89a5cf9699417018653eb7b62595252cfaa714a
muffinshades <89941934+MuffinShades@users.noreply.github.com>
2025-02-17 21:10:52 -0500

    Update bitstream.hpp

--- src/bitstream.hpp
+++ src/bitstream.hpp
@@ -19,18 +19,27 @@ extern "C" {
 class BitStream : public ByteStream {
 private:
     size_t subPos = 0;
+
 protected:
     using ByteStream::skip;
+	MSFL_EXP void pos_adv() override;
 public:
     MSFL_EXP void writeBytes(byte* dat, size_t sz) override;
 	MSFL_EXP void writeInt(i64 val, size_t nBytes = 4) override;
 	MSFL_EXP void writeUInt(u64 val, size_t nBytes = 4) override;
 	MSFL_EXP void writeByte(byte b) override;
 
+	MSFL_EXP void writeBit(bit b);
+	MSFL_EXP void writeBits(u64 val, size_t nBits);
+
     MSFL_EXP byte* readBytes(size_t sz) override;
 	MSFL_EXP i64 readInt(size_t nBytes) override;
 	MSFL_EXP u64 readUInt(size_t nBytes) override;
 	MSFL_EXP byte readByte() override;
+
+	MSFL_EXP bit readBit();
+	MSFL_EXP u64 readBits(size_t nBits);
+	MSFL_EXP u64 nextBits(size_t nBits);
 
     MSFL_EXP void skipBytes(size_t nBytes);
     MSFL_EXP void skipBits(size_t nBits);
