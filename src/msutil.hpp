cf66e92014c9cf12e0a4fb52bf689b941a0fcb57
muffinshades <89941934+MuffinShades@users.noreply.github.com>
2025-02-18 21:24:02 -0500

    Update msutil.hpp

--- src/msutil.hpp
+++ src/msutil.hpp
@@ -284,24 +284,6 @@ static i32 fast_log16(i32 val) __log_def(4)
 static i32 fast_log32(i32 val) __log_def(5)
 static i32 fast_log64(i32 val) __log_def(6)
 
-static u64 endian_swap(u64 val, size_t nBytes) {
-    switch(nBytes) {
-        case 2:
-            return ((val & 0xff00) >> 8) | ((val & 0xff) << 8);
-        case 3:
-            return ((val & 0xff0000) >> 16) | ((val & 0xff) << 16);
-        case 4: 
-            return ((val & 0xff) << 24) | ((val & 0xff00) << 8) | ((val & 0xff0000) >> 8) | ((val & 0xff000000) >> 24);
-        case 8:
-            return ((val & 0x00000000000000ffULL) << 56) | ((val & 0xff00000000000000ULL) >> 56) |
-              ((val & 0x000000000000ff00ULL) << 40) | ((val & 0x00ff000000000000ULL) >> 40) |
-              ((val & 0x0000000000ff0000ULL) << 24) | ((val & 0x0000ff0000000000ULL) >> 24) |
-              ((val & 0x00000000ff000000ULL) <<  8) | ((val & 0x000000ff00000000ULL) >> 8);
-        default:
-            return val;
-    }
-}
-
 
 static u64 endian_swap(u64 val, size_t nBytes) {
     switch (nBytes) {
